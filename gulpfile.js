536e12c6c74d275850adcb4f23aaee8fc726fc0c
baihaiyang <haiyangbai2@creditease.cn>
2019-05-07 17:18:11 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -7,26 +7,37 @@ var autoprefix = new LessAutoprefix({ browsers: ['last 2 versions'] });
 const cssentry = "./src/styles/*.less";
 const htmlentry = './src/index.html';
 // 开发环境
-function buildcss(){
-//   return watch(cssentry, { ignoreInitial:false }, function(){
+function devcss(){
+  return watch(cssentry, { ignoreInitial:false }, function(){
     gulp.src(cssentry)
     .pipe(less({
         plugins: [autoprefix],
         paths: [ path.join(__dirname, 'src', 'includes') ]
     }))
     .pipe(gulp.dest('./dist'));
-//   })
+  })
 }
-function buildhtml(){
-    // return watch(htmlentry, { ignoreInitial:false }, function(){
-      gulp.src(htmlentry)
-      .pipe(gulp.dest('./dist'));
-    // })
-  }
-watch([htmlentry, cssentry], function(){
-    buildhtml();
-    buildcss();
-    console.log(111)
-});
-let build = gulp.parallel(buildhtml, buildcss);
+function devhtml(){
+  return watch(htmlentry, { ignoreInitial:false }, function(){
+    gulp.src(htmlentry)
+    .pipe(gulp.dest('./dist'));
+  })
+}
+// 生产环境
+function prodcss(){
+  gulp.src(cssentry)
+  .pipe(less({
+      plugins: [autoprefix],
+      paths: [ path.join(__dirname, 'src', 'includes') ]
+  }))
+  .pipe(gulp.dest('./dist'));
+}
+function prodhtml(){
+  gulp.src(htmlentry)
+  .pipe(gulp.dest('./dist'));
+}
+let build = gulp.parallel(devhtml, devcss);
+if(process.env.NODE_ENV == 'production'){
+  build = gulp.parallel(prodhtml, prodcss);
+}
 gulp.task("default", build);
